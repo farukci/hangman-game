63cdf7babf6fcc37bbf5e52c0cdf92e83c1f33d3
farukci <ferruhcihan20@gmail.com>
2020-09-27 19:26:52 +0300

    first commit

---
+++ myScript.js
@@ -0,0 +1,3 @@
+const words = ["javascript", "function", "arguments", "debugger", "extends", "package", "protected", "continue", "boolean", "string", "undefined", "switch", "volatile"];
+
+console.log(words);
