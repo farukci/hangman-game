867bd179bdbc502e1b635f56b0c148985f64aa65
farukci <ferruhcihan20@gmail.com>
2020-09-30 22:31:13 +0300

    js updated

--- myScript.js
+++ myScript.js
@@ -1,3 +1,15 @@
 const words = ["javascript", "function", "arguments", "debugger", "extends", "package", "protected", "continue", "boolean", "string", "undefined", "switch", "volatile"];
 
-console.log(words);
+// document.getElementById("first").innerHTML = words[0];
+
+window.addEventListener("DOMContentLoaded", selectWord());
+
+
+function selectWord() {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    // document.querySelector("#first").textContent = words[randomIndex];
+    let node = document.createElement("p");
+    
+}
+
+
